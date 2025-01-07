fcd2650763d37572ce260666c498ce2455457ecf
Kevin Castro <kcapupp@princeton.edu>
2025-01-06 21:17:04 -0500

    HeapButtons

---
+++ algo-lens-react/src/Components/HeapButtons.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { MAX_SIZE } from "../utils/nodeArrays";
+
+const HeapButtons = ({
+  heap,
+  reset,
+  add,
+  remove,
+  executeNextStep,
+  stepByStepMode,
+  stepsToExecute,
+}) => {
+  return (
+    <div>
+      <button onClick={() => reset()}>Reset</button>
+      <button
+        onClick={() => add(Math.floor(Math.random() * 100))}
+        disabled={heap.length >= MAX_SIZE} // Disabled when heap length is >= MAX_SIZE
+      >
+        Add Random
+      </button>
+      <button
+        onClick={() => remove()}
+        disabled={heap.length === 0} // Disabled when heap is empty
+      >
+        Remove Min
+      </button>
+
+      {stepByStepMode && stepsToExecute.length > 0 && (
+        <button onClick={executeNextStep}>Next Step</button>
+      )}
+    </div>
+  );
+};
+
+export default HeapButtons;
