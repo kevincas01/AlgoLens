f3747c4a266389076406ae9637c0c28cfb00fd52
Kevin Castro <kcapupp@princeton.edu>
2025-02-03 17:37:03 -0500

    rename var

--- algo-lens-react/src/Algorithms/Sorting.js
+++ algo-lens-react/src/Algorithms/Sorting.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
+import { mergeSort, quickSort } from "../utils/sortingAlgorithms";
 const MIN_BAR_WIDTH = 25; // Minimum width of each bar in pixels
 const BAR_MARGIN = 4;
 const Sorting = ({ stepByStepMode }) => {
   const containerRef = useRef(null);
   const [array, setArray] = useState([]);
-  const [stepsToTake, setStepsToTake] = useState([]);
+  const [stepsToExecute, setStepsToExecute] = useState([]);
   const [currentStep, setCurrentStep] = useState(0);
 
   const MAX_BARS = 100;
@@ -36,27 +37,34 @@ const Sorting = ({ stepByStepMode }) => {
   }, []);
 
   useEffect(() => {
-    if (!stepByStepMode && stepsToTake.length > 0) {
+    if (!stepByStepMode && stepsToExecute.length > 0) {
       const intervalId = setInterval(() => {
         executeNextStep();
       }, 50); // Execute every 500ms
 
-      // Clear the interval when all stepsToTake are executed or if the mode is changed
-      if (currentStep > stepsToTake.length || stepsToTake.length == 0) {
+      // Clear the interval when all stepsToExecute are executed or if the mode is changed
+      if (currentStep > stepsToExecute.length || stepsToExecute.length == 0) {
         clearInterval(intervalId);
       }
 
       return () => clearInterval(intervalId);
     }
-  }, [stepByStepMode, currentStep, stepsToTake]);
+  }, [stepByStepMode, currentStep, stepsToExecute]);
 
   const executeNextStep = () => {
-    if (currentStep < stepsToTake.length && stepsToTake.length !== 0) {
-      const step = stepsToTake[currentStep];
+    if (currentStep < stepsToExecute.length && stepsToExecute.length !== 0) {
+      const step = stepsToExecute[currentStep];
 
       if (step.type === "overwrite") {
         const newArr = [...array];
         newArr[step.index] = step.value;
+        setArray(newArr);
+      }
+      if (step.type === "swap") {
+        const newArr = [...array];
+        const [i, j] = step.indices;
+        // Swap the elements
+        [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
         setArray(newArr);
       }
 
@@ -64,65 +72,16 @@ const Sorting = ({ stepByStepMode }) => {
     }
   };
 
-  const mergeSort = (arr) => {
-    let tempSteps = [];
-    let sorted = arr.slice();
-
-    const merge2 = (leftIndex1, rightIndex1, leftIndex2, rightIndex2) => {
-      let temp = [];
-      let i = leftIndex1,
-        j = leftIndex2;
-
-      while (i <= rightIndex1 && j <= rightIndex2) {
-        tempSteps.push({ type: "compare", indices: [i, j] }); // Compare
-
-        if (sorted[i] < sorted[j]) {
-          temp.push(sorted[i]);
-          i++;
-        } else {
-          temp.push(sorted[j]);
-          j++;
-        }
-      }
-
-      while (i <= rightIndex1) {
-        tempSteps.push({ type: "compare", indices: [i, i] }); // Single element left
-        temp.push(sorted[i]);
-        i++;
-      }
-
-      while (j <= rightIndex2) {
-        tempSteps.push({ type: "compare", indices: [j, j] });
-        temp.push(sorted[j]);
-        j++;
-      }
-
-      for (let k = 0; k < temp.length; k++) {
-        tempSteps.push({
-          type: "overwrite",
-          index: leftIndex1 + k,
-          value: temp[k],
-        });
-        sorted[leftIndex1 + k] = temp[k];
-      }
-    };
-
-    const divide2 = (leftIndex, rightIndex) => {
-      if (leftIndex >= rightIndex) return;
-
-      const mid = Math.floor((leftIndex + rightIndex) / 2);
-      divide2(leftIndex, mid);
-      divide2(mid + 1, rightIndex);
-      merge2(leftIndex, mid, mid + 1, rightIndex);
-    };
-
-    divide2(0, arr.length - 1);
-    return tempSteps;
-  };
-
   const startMergeSort = () => {
     const newSteps = mergeSort(array);
-    setStepsToTake(newSteps);
+    setStepsToExecute(newSteps);
+    setCurrentStep(0);
+  };
+
+  const startQuickSort = () => {
+    const newSteps = quickSort(array);
+    console.log(newSteps);
+    setStepsToExecute(newSteps);
     setCurrentStep(0);
   };
 
@@ -134,16 +93,23 @@ const Sorting = ({ stepByStepMode }) => {
             <div
               key={index}
               className={`sort-bar ${
-                stepsToTake[currentStep]?.type === "compare" &&
-                stepsToTake[currentStep].indices.includes(index)
+                stepsToExecute[currentStep]?.type === "compare" &&
+                stepsToExecute[currentStep].indices.includes(index)
                   ? "active-compare"
                   : ""
               }
 
               ${
-                stepsToTake[currentStep]?.type === "overwrite" &&
-                stepsToTake[currentStep]?.index == index + 1
+                stepsToExecute[currentStep]?.type === "overwrite" &&
+                stepsToExecute[currentStep]?.index == index + 1
                   ? "active-overwrite"
+                  : ""
+              }
+
+              ${
+                stepsToExecute[currentStep]?.type === "swap" &&
+                stepsToExecute[currentStep]?.indices.includes(index)
+                  ? "active-swap"
                   : ""
               }
               
@@ -151,14 +117,23 @@ const Sorting = ({ stepByStepMode }) => {
               style={{
                 height: `${element}%`,
                 width: `${MIN_BAR_WIDTH}px`,
-                minWidth:"5px"
+                minWidth: "5px",
               }}
             ></div>
           );
         })}
       </div>
       <button onClick={() => getNewArray()}>Reset Array</button>
-      <button onClick={startMergeSort}>Start Merge Sort</button>
+      <button onClick={startMergeSort}>Merge Sort</button>
+      <button onClick={startQuickSort}>Quick Sort</button>
+      {stepByStepMode && stepsToExecute.length > 0 && (
+        <>
+          <button disabled={currentStep === 0}>Previous Step</button>
+          <button onClick={executeNextStep}>
+            {currentStep === stepsToExecute.length ? "Finish" : "Next Step"}
+          </button>
+        </>
+      )}
     </div>
   );
 };
